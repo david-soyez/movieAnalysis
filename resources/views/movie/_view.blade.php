70fda721ca7247e7e0fcd315cacb30f28981f1e3
David Soyez <david@dexnode.com>
2016-05-01 00:38:46 +0200

    Graph

--- resources/views/movie/_view.blade.php
+++ resources/views/movie/_view.blade.php
@@ -14,19 +14,31 @@ function drawRare{{ $movie->id}}() {
 
       data.addRows([
         @foreach ($conversations as $conversation)
-        <?php for($i=$conversation->timeline_start/ 1000;$i<=$conversation->timeline_end/ 1000;$i++):?>
-        [{{ $i/60 }},      {{ (float)($conversation->score) }},      {{ (float)($conversation->readingspeed) }}],
-        <?php endfor; ?>
+        <?php 
+            if(isset($end)) {
+                for($i=$end/1000;$i<$conversation->timeline_start/1000;$i++) {
+                ?>
+                    [{{ $i/60 }}, 0, {{ $endReadingSpeed }}],
+                <?php
+                }
+            }
+            for($i=$conversation->timeline_start/ 1000/6;$i<=$conversation->timeline_end/ 1000/6;$i++):?>
+        [{{ $i/10 }},      {{ (float)($conversation->score) }},      {{ (float)($conversation->readingspeed) }}],
+<?php 
+            $end=$conversation->timeline_end+1; 
+            $endReadingSpeed = $conversation->readingspeed;
+            
+            endfor; ?>
         @endforeach
       ]);
 
       var options = {
-      bar: {groupWidth: "90%"},
+        bar: {groupWidth: 90},
         hAxis: {
           title: '{{ $movie->title}}'
         },
         seriesType: 'bars',
-        series: {1: {type: 'line',targetAxisIndex:1},0: {type: 'bar',targetAxisIndex:0}},
+        series: {1: {type: 'line',targetAxisIndex:1, lineDashStyle: [2, 2] },0: {type: 'bar',targetAxisIndex:0}},
         vAxes: { 0: {logScale: false,maxValue: 10000}, 1: {logScale: false,maxValue: 40} },
         vAxis: {
           title: 'Conversation Complexity score',
