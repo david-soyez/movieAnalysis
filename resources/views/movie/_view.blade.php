9262080b37b5df7152b2c105da3effc4acb59cba
David Soyez <david@dexnode.com>
2016-04-30 23:59:29 +0200

    Graph

--- resources/views/movie/_view.blade.php
+++ resources/views/movie/_view.blade.php
@@ -10,11 +10,12 @@ function drawRare{{ $movie->id}}() {
       var data = new google.visualization.DataTable();
       data.addColumn('number', 'X');
       data.addColumn('number', 'Conversation complexity');
+      data.addColumn('number', 'Speed');
 
       data.addRows([
         @foreach ($conversations as $conversation)
         <?php for($i=$conversation->timeline_start/ 1000;$i<=$conversation->timeline_end/ 1000;$i++):?>
-        [{{ $i/60 }},      {{ (float)($conversation->score ) }}],
+        [{{ $i/60 }},      {{ (float)($conversation->score) }},      {{ (float)($conversation->readingspeed) }}],
         <?php endfor; ?>
         @endforeach
       ]);
@@ -24,16 +25,17 @@ function drawRare{{ $movie->id}}() {
         hAxis: {
           title: '{{ $movie->title}}'
         },
+        seriesType: 'bars',
+        series: {1: {type: 'line',targetAxisIndex:1},0: {type: 'bar',targetAxisIndex:0}},
+        vAxes: { 0: {logScale: false,maxValue: 10000}, 1: {logScale: false,maxValue: 40} },
         vAxis: {
           title: 'Conversation Complexity score',
               viewWindow: {
-              min:0,
-              max: 10000
             }
         }
       };
 
-      var chart = new google.visualization.ColumnChart(document.getElementById('chart_divRare{{ $movie->id}}'));
+      var chart = new google.visualization.ComboChart(document.getElementById('chart_divRare{{ $movie->id}}'));
 
       chart.draw(data, options);
     }
